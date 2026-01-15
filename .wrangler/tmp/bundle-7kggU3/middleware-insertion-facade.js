5cb255452ddc3e4de973aac416898b292228ad71
Laitei <teiteipara@gmail.com>
2026-01-15 20:31:07 +0630

    Added middleware for Cloudflare Workers and new icons for various themes

--- .wrangler/tmp/bundle-7kggU3/middleware-insertion-facade.js
+++ .wrangler/tmp/bundle-7kggU3/middleware-insertion-facade.js
@@ -1,8 +1,8 @@
-				import worker, * as OTHER_EXPORTS from "C:\\Users\\laite\\Documents\\TSD\\.wrangler\\tmp\\pages-y3JLiy\\functionsWorker-0.4224942472623039.mjs";
+				import worker, * as OTHER_EXPORTS from "C:\\Users\\laite\\Documents\\TSD\\.wrangler\\tmp\\pages-HzYenb\\functionsWorker-0.8018803063890997.mjs";
 				import * as __MIDDLEWARE_0__ from "C:\\Users\\laite\\AppData\\Roaming\\npm\\node_modules\\wrangler\\templates\\middleware\\middleware-ensure-req-body-drained.ts";
 import * as __MIDDLEWARE_1__ from "C:\\Users\\laite\\AppData\\Roaming\\npm\\node_modules\\wrangler\\templates\\middleware\\middleware-miniflare3-json-error.ts";
 
-				export * from "C:\\Users\\laite\\Documents\\TSD\\.wrangler\\tmp\\pages-y3JLiy\\functionsWorker-0.4224942472623039.mjs";
+				export * from "C:\\Users\\laite\\Documents\\TSD\\.wrangler\\tmp\\pages-HzYenb\\functionsWorker-0.8018803063890997.mjs";
 				const MIDDLEWARE_TEST_INJECT = "__INJECT_FOR_TESTING_WRANGLER_MIDDLEWARE__";
 				export const __INTERNAL_WRANGLER_MIDDLEWARE__ = [
 					
