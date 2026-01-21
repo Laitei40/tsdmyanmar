a87d6aa39255775c5ceca2918983d2b86e86fa55
Laitei <teiteipara@gmail.com>
2026-01-21 23:09:42 +0630

    modified:   .wrangler/state/v3/d1/miniflare-D1DatabaseObject/27b90a706bdbef4235ac33f862a612d1b2ff78728061afbde7360bb253d038ed.sqlite 	renamed:    .wrangler/tmp/bundle-A48Nm3/middleware-insertion-facade.js -> .wrangler/tmp/bundle-3HQ9so/middleware-insertion-facade.js 	renamed:    .wrangler/tmp/bundle-A48Nm3/middleware-loader.entry.ts -> .wrangler/tmp/bundle-3HQ9so/middleware-loader.entry.ts 	new file:   .wrangler/tmp/bundle-IA2qRV/middleware-insertion-facade.js 	renamed:    .wrangler/tmp/bundle-Hlj5AR/middleware-loader.entry.ts -> .wrangler/tmp/bundle-IA2qRV/middleware-loader.entry.ts 	renamed:    .wrangler/tmp/bundle-Hlj5AR/middleware-insertion-facade.js -> .wrangler/tmp/bundle-IiLxjk/middleware-insertion-facade.js 	new file:   .wrangler/tmp/bundle-IiLxjk/middleware-loader.entry.ts 	new file:   .wrangler/tmp/bundle-Tf2iDP/middleware-insertion-facade.js 	new file:   .wrangler/tmp/bundle-Tf2iDP/middleware-loader.entry.ts 	new file:   .wrangler/tmp/bundle-qrfIHC/middleware-insertion-facade.js 	new file:   .wrangler/tmp/bundle-qrfIHC/middleware-loader.entry.ts 	new file:   .wrangler/tmp/bundle-sVugKJ/middleware-insertion-facade.js 	new file:   .wrangler/tmp/bundle-sVugKJ/middleware-loader.entry.ts 	renamed:    .wrangler/tmp/dev-TgKV9G/functionsWorker-0.9986435896647681.js -> .wrangler/tmp/dev-Kqhtqg/functionsWorker-0.3295199995620509.js 	renamed:    .wrangler/tmp/dev-TgKV9G/functionsWorker-0.9986435896647681.js.map -> .wrangler/tmp/dev-Kqhtqg/functionsWorker-0.3295199995620509.js.map 	new file:   .wrangler/tmp/dev-OWZvE0/functionsWorker-0.6548067218898882.js 	new file:   .wrangler/tmp/dev-OWZvE0/functionsWorker-0.6548067218898882.js.map 	new file:   .wrangler/tmp/dev-c3Y2wg/functionsWorker-0.6644005792634483.js 	new file:   .wrangler/tmp/dev-c3Y2wg/functionsWorker-0.6644005792634483.js.map 	renamed:    .wrangler/tmp/pages-kzYOzw/functionsRoutes-0.5792567236945432.mjs -> .wrangler/tmp/pages-K1twh8/functionsRoutes-0.0747066772174565.mjs    renamed:    .wrangler/tmp/pages-kzYOzw/functionsWorker-0.9986435896647681.mjs -> .wrangler/tmp/pages-K1twh8/functionsWorker-0.6644005792634483.mjs 	renamed:    .wrangler/tmp/pages-kzYOzw/functionsWorker-0.9986435896647681.mjs.map -> .wrangler/tmp/pages-K1twh8/functionsWorker-0.6644005792634483.mjs.map 	new file:   .wrangler/tmp/pages-RqPZ7g/functionsRoutes-0.6178302509296495.mjs 	new file:   .wrangler/tmp/pages-RqPZ7g/functionsWorker-0.6548067218898882.mjs 	new file:   .wrangler/tmp/pages-RqPZ7g/functionsWorker-0.6548067218898882.mjs.map 	new file:   .wrangler/tmp/pages-U5CFy3/functionsRoutes-0.5705057014714657.mjs 	new file:   .wrangler/tmp/pages-U5CFy3/functionsWorker-0.3295199995620509.mjs 	new file:   .wrangler/tmp/pages-U5CFy3/functionsWorker-0.3295199995620509.mjs.map

---
+++ .wrangler/tmp/bundle-IiLxjk/middleware-insertion-facade.js
@@ -0,0 +1,11 @@
+				import worker, * as OTHER_EXPORTS from "C:\\Users\\laite\\Documents\\TSD\\.wrangler\\tmp\\pages-K1twh8\\functionsWorker-0.6644005792634483.mjs";
+				import * as __MIDDLEWARE_0__ from "C:\\Users\\laite\\AppData\\Roaming\\npm\\node_modules\\wrangler\\templates\\middleware\\middleware-ensure-req-body-drained.ts";
+import * as __MIDDLEWARE_1__ from "C:\\Users\\laite\\AppData\\Roaming\\npm\\node_modules\\wrangler\\templates\\middleware\\middleware-miniflare3-json-error.ts";
+
+				export * from "C:\\Users\\laite\\Documents\\TSD\\.wrangler\\tmp\\pages-K1twh8\\functionsWorker-0.6644005792634483.mjs";
+				const MIDDLEWARE_TEST_INJECT = "__INJECT_FOR_TESTING_WRANGLER_MIDDLEWARE__";
+				export const __INTERNAL_WRANGLER_MIDDLEWARE__ = [
+					
+					__MIDDLEWARE_0__.default,__MIDDLEWARE_1__.default
+				]
+				export default worker;
